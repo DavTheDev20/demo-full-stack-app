7ee8287245b8bb33859b6c161a5f0663a2f37095
DavTheDev20 <reiddavin@outlook.com>
2022-03-22 22:01:26 -0400

    Deployment error 5 fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 require('./utils/dbConnect').connect();
 const usersRouter = require('./routes/usersRouter');
 const postsRouter = require('./routes/postsRouter');
@@ -13,7 +12,10 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
 
 app.use('/api/user', usersRouter);
 
